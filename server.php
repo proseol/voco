995b17c0cb747d81c5a28167aac02be1913bab6a
Sergei <proseol@users.noreply.github.com>
2024-01-23 15:57:58 +0200

    fix server.php, add install function
    Add install function to Server class

--- server.php
+++ server.php
@@ -44,6 +44,25 @@ class Server
         return "0";
     }
 
+    public function install() {
+        // Create user table if not exists
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+                    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(30) NOT NULL,
+                    username VARCHAR(30) NOT NULL
+                )";
+
+        if ($this->conn->query($sql)) {
+            echo "Installed successfully";
+            return true;
+        }
+
+        // Close the connection
+        $this->close();
+
+        return false;
+    }
+
     public function close() {
         $this->conn->close();
     }
